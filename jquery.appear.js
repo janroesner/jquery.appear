dc8ec72f56d7bbd41ae52e2ac707e1ead682463e
Jan Roesner <jan@roesner.it>
2014-10-23 14:38:29 +0200

    Calling appear() on multiple DOM objects caused wrong $disappeared calculation.
    The reason was, that $prior_appeared did not persist correctly on case appear() was called on more that one selector. Simply fixed by making $prior_appeared's to persist by pushing them instead of setting them.

--- jquery.appear.js
+++ jquery.appear.js
@@ -34,7 +34,7 @@
         var $disappeared = $prior_appeared.not($appeared);
         $disappeared.trigger('disappear', [$disappeared]);
       }
-      $prior_appeared = $appeared;
+      $prior_appeared.push($appeared);
     }
   }
 
